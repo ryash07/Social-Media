9f8a0b3d8cb91df747a9941b10e9f9fe764baa3b
Ryash07 <ryash4170@gmail.com>
2023-07-06 13:15:02 +0530

    Leftbar and RighBar constructed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,23 @@ import './home.scss'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </div>
   )
 }
 export default Home
